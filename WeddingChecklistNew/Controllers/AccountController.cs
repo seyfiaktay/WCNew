27bf32093f5cccd0fb91a8b2f6e3182959fa79d6
seyfiaktay <seyfiaktay89@hotmail.com>
2019-05-28 16:45:44 +0300

    login && register && logout

--- WeddingChecklistNew/Controllers/AccountController.cs
+++ WeddingChecklistNew/Controllers/AccountController.cs
@@ -1,15 +1,32 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web;
 using System.Web.Mvc;
+using WeddingChecklistNew.Models;
 
 namespace WeddingChecklistNew.Controllers
 {
     public class AccountController : Controller
     {
+        private APIAccountController mAPIAccountController = new APIAccountController();
         // GET: Account
         public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Logout()
         {
             return View();
         }
--- WeddingChecklistNew/Controllers/HomeController.cs
+++ WeddingChecklistNew/Controllers/HomeController.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WeddingChecklistNew.Models;
 
 namespace WeddingChecklistNew.Controllers
 {
     public class HomeController : Controller
     {
+        private APIAccountController mAPIAccountController = new APIAccountController();
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
-
             return View();
         }
     }
